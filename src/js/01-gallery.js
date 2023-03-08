74c81c36274003ab936198765bd9e4ea187b452d
Mykkolla <tarabukin.n.10@gmail.com>
2023-03-08 15:11:37 +0200

    add firsttusck

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryItemsTable = galleryItems.map(({preview , original, description}) => 
+`<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+).join('');
+
+
+const galleryMain = document.querySelector('.gallery');
+
+galleryMain.insertAdjacentHTML('beforeend', galleryItemsTable);
+
+
+const hendlerClickOnImg = (e) => {
+    // preventDefault
+        e.preventDefault();}
+
+ galleryMain.addEventListener('click', hendlerClickOnImg)
+
+ let gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250});
+
+
+
