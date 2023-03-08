b213e67dfae40d9492796dd9d0c56a5fede363a2
Mykkolla <tarabukin.n.10@gmail.com>
2023-03-08 16:08:05 +0200

    add secondtask

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+console.log(Player)
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+const onPlay = function(data) {
+
+    window.localStorage["videoplayer-current-time"] = data.seconds;
+    console.log(localStorage["videoplayer-current-time"]);
+};
+
+
+player.on('timeupdate', throttle(onPlay, 1500));
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+
+
+
+player.setCurrentTime(localStorage["videoplayer-current-time"]).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
 
