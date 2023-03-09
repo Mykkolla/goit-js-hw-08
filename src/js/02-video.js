de89d8a3fc491ec1f80c123877ce498a022ce84e
Mykkolla <tarabukin.n.10@gmail.com>
2023-03-09 17:17:41 +0200

    rr

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,29 +1,27 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-// console.log(Player)
-
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 
 const onPlay = function(data) {
-
-    window.localStorage["videoplayer-current-time"] = data.seconds;
-    console.log(localStorage["videoplayer-current-time"]);
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
+    // window.localStorage["videoplayer-current-time"] = data.seconds;
+    // console.log(localStorage["videoplayer-current-time"]);
+ 
 };
 
+const time = localStorage.getItem("videoplayer-current-time");
+const timePars = JSON.parse(time);
+console.log(timePars); 
 
 player.on('timeupdate', throttle(onPlay, 1500));
 
-player.getVideoTitle().then(function(title) {
-    // console.log('title:', title);
-});
 
 
 
-
-player.setCurrentTime(localStorage["videoplayer-current-time"]).then(function(seconds) {
+player.setCurrentTime(timePars).then(function(seconds) {
 }).catch(function(error) {
     switch (error.name) {
         case 'RangeError':
