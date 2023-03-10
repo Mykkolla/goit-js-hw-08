9ff0fc423f63c83eefd27ff837b67cd54c07e86c
Mykkolla <tarabukin.n.10@gmail.com>
2023-03-10 16:18:05 +0200

    addtask3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -40,7 +40,8 @@ function returnTextereaForm () {
         formText.value = parsedSettings.message;
         formInput.value = parsedSettings.email;
     }
-
+    formText.value = "type message";
+    formInput.value = "type email";
 
 
 }
